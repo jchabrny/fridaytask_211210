71599df1f78aa50fbc70300ed69aacd668e80914
Jannis Aziz Khammas <aziz_khammas@yahoo.de>
2021-12-10 10:21:00 +0100

    Initial commit Project setup

---
+++ src/main/java/ShopService.java
@@ -0,0 +1,28 @@
+public class ShopService {
+
+    public static void main(String[] args) {
+
+        // Create ProductRepo
+        // Create OrderRepo
+
+        // Create product 1
+        // Create product 2
+        // Create product 3
+
+        // Add products to ProductRepo
+
+        // List Product 2
+
+        // List all products
+
+        // Create order 1
+        // Create order 2
+        // Create order 3
+
+        // Add orders to OrderRepo
+
+        // List order 2 by ID
+
+        // List all orders
+    }
+}
